12cbed9716b028d0b27b2eb5ca88477d62229736
Tiago Souza <tiago.souza@zup.com.br>
2019-07-05 17:28:28 -0300

    Criar projeto empresa Tourzup

---
+++ com.br.Tourzup/src/main/java/com/br/Tourzup/Controllers/tourzupController.java
@@ -0,0 +1,8 @@
+package com.br.Tourzup.Controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class tourzupController {
+
+}
---
+++ com.br.Tourzup/src/main/java/com/br/Tourzup/Services/tourzupService.java
@@ -0,0 +1,8 @@
+package com.br.Tourzup.Services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class tourzupService {
+
+}
